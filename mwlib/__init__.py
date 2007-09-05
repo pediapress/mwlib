fa6014d5eadd425abf8958eff3d019b2aabb7088
ralf@brainbot.com <ralf@brainbot.com>
2007-09-05 15:37:58 +0200

    fix

--- mwlib/__init__.py
+++ mwlib/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) 2007, PediaPress GmbH
 # See README.txt for additional licensing information.
 
-from mwlib.log import Log
 import pkg_resources
 pkg_resources.declare_namespace("mwlib")
+from mwlib.log import Log
