b40c11f9f20c740372f002427bb8746544faaf24
Johannes Beigel <johannes.beigel@brainbot.com>
2007-09-24 14:53:53 +0200

    use metabook zipfile format

--- mwlib/zipwiki.py
+++ mwlib/zipwiki.py
@@ -3,42 +3,69 @@
 # Copyright (c) 2007, PediaPress GmbH
 # See README.txt for additional licensing information.
 
+import collections
 import os
-import zipfile
 import simplejson
 import tempfile
+import zipfile
+
+from mwlib.metabook import MetaBook
 
 class Wiki(object):
     def __init__(self, path):
         self.zf = zipfile.ZipFile(path)
-        c = simplejson.loads(self.zf.read("contents.json"))
-        self.articles = c['articles']
-        self.templates = c['templates']
-
-    def getRawArticle(self, name):
-        return self.articles[name]
-
+        mb = MetaBook()
+        mb.loadJson(self.zf.read("collection.json"))
+        self.articles = collections.defaultdict(list)
+        for item in mb.getItems():
+            if item['type'] != 'article':
+                continue
+            self.articles[item['title']].append(item)
+        self.templates = {}
+    
+    def _getArticle(self, title, revision=None):
+        articles = sorted(self.articles[title], key=lambda item: item.get('revision', None), reverse=True)
+        if not articles:
+            return None
+        if revision is None:
+            return articles[0]
+        for article in articles:
+            if article.get('revision', None) == revision:
+                return article
+        return None
+    
+    def getRawArticle(self, title, revision=None):
+        article = self._getArticle(title, revision=revision)
+        return article['content']
+    
+    def getURL(self, title, revision=None):
+        article = self._getArticle(title, revision=revision)
+        return article['url']
+    
     def getTemplate(self, name, followRedirects=True):
         return self.templates[name]
+    
 
 class ImageDB(object):
-    def __init__(self, path):
-        self.zf = zipfile.ZipFile(path)
-        self._tmpdir = None
-
+    def __init__(self, zipfilename, tmpdir=None):
+        self.zf = zipfile.ZipFile(zipfilename)
+        self._tmpdir = tmpdir
+        mb = MetaBook()
+        mb.loadJson(self.zf.read('collection.json'))
+        self.imageMap = mb.imageMap
+    
     @property
     def tmpdir(self):
         if self._tmpdir is None:
             self._tmpdir = unicode(tempfile.mkdtemp())
-
         return self._tmpdir
 
     def getDiskPath(self, name):
         try:
-            data = self.zf.read((u"images/%s" % name).encode("utf-8"))
+            data = self.zf.read(self.imageMap[name].encode('utf-8'))
         except KeyError: # no such file
             return None
-
+        
         res = os.path.join(self.tmpdir, name)
         f=open(res, "wb")
         f.write(data)
