fbb58d8ebfb7961adf22f556205b36c2b56cfeef
ralf@brainbot.com <ralf@brainbot.com>
2007-10-01 23:00:18 +0200

    add test for template expansion inside <pre> tags.

---
+++ tests/test_expander.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env py.test
+
+from mwlib import expander
+
+class DictDB(object):
+    def __init__(self, d):
+        self.d = d
+
+    def getRawArticle(self, title):
+        return self.d[title]
+
+    def getTemplate(self, title, dummy):
+        return self.d.get(title, u"")
+
+
+
+def test_noexpansion_inside_pre():
+    db = DictDB(dict(Art="<pre>A{{Pipe}}B</pre>",
+                     Pipe="C"))
+
+    te = expander.Expander(db.getRawArticle("Art"), pagename="thispage", wikidb=db)
+    res = te.expandTemplates()
+
+    
+    print "EXPANDED:", repr(res)
+    assert u"ACB" not in res
+    assert u"A{{Pipe}}B" in res
