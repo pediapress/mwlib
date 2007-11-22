92148f60dfdc9bb02f7ef5d77df8ce919cd8f868
ralf@brainbot.com <ralf@brainbot.com>
2007-11-22 13:38:34 +0100

    wtf: use ez_setup.py from simplejson.

--- ez_setup.py
+++ ez_setup.py
@@ -60,7 +60,7 @@ def _validate_md5(egg_name, data):
 
 
 def use_setuptools(
-    version=DEFAULT_VERSION, download_base=DEFAULT_URL, to_dir=os.curdir,
+    version=DEFAULT_VERSION, download_base=DEFAULT_URL, to_dir=os.curdir, min_version=None,
     download_delay=15
 ):
     """Automatically find/download setuptools and make it available on sys.path
@@ -89,7 +89,9 @@ def use_setuptools(
 
     import pkg_resources
     try:
-        pkg_resources.require("setuptools>="+version)
+        if not min_version:
+            min_version = version
+        pkg_resources.require("setuptools>="+min_version)
 
     except pkg_resources.VersionConflict, e:
         # XXX could we install in a subprocess here?
@@ -97,7 +99,7 @@ def use_setuptools(
             "The required version of setuptools (>=%s) is not available, and\n"
             "can't be installed while this script is running. Please install\n"
             " a more recent version first.\n\n(Currently using %r)"
-        ) % (version, e.args[0])
+        ) % (min_version, e.args[0])
         sys.exit(2)
 
 def download_setuptools(
