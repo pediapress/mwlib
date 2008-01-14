1717de3eb101d9490a0f4397686bda75a0d404bb
ralf@brainbot.com <ralf@brainbot.com>
2008-01-14 17:32:01 +0100

    better test

--- tests/test_imgmap.py
+++ tests/test_imgmap.py
@@ -3,5 +3,15 @@
 from mwlib import imgmap
 
 def test_whitespace_line():
-    s="image:large.png|Map\n  poly 642 127 615 162 635 205 [[Drenthe]]\n"
-    imgmap.ImageMapFromString(s)
+    s="""image:large.png|Map
+# XX
+poly 642 127 615 162 635 205 [[Drenthe]]
+"""
+    map1=imgmap.ImageMapFromString(s)
+    print "MAP1:", map1
+    map2=imgmap.ImageMapFromString(s.replace("# XX", "  "))
+    print "MAP2:", map2
+
+    assert map2.image, "missing image"
+    assert map2.entries, "missing entries"
+
