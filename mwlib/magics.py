31fdc44f06d0d28237f4ef901258630030a07c3d
ralf@brainbot.com <ralf@brainbot.com>
2008-04-21 08:57:26 +0200

    implement iferror magic function

--- mwlib/magics.py
+++ mwlib/magics.py
@@ -272,7 +272,9 @@ class StringMagic(object):
 
 class ParserFunctions(object):
     wikidb = None
-
+    def _error(self,s):
+        return '<strong class="error">%s</strong>' % (s,)
+    
     def TAG(self, args):
         name = args[0].strip()
         r= u"<%s>%s</%s>" % (name, args[1], name)
@@ -318,7 +320,7 @@ class ParserFunctions(object):
             try:
                 r=str(expr.expr(rl[0]))
             except Exception, err:
-                return str(err)
+                return self._error(err)
 
             if "e" in r:
                 f,i = r.split("e")
@@ -332,7 +334,7 @@ class ParserFunctions(object):
         try:
             r = expr.expr(rl[0])
         except Exception, err:
-            return str(err)
+            return self._error(err)
 
         if r:
             return rl[1]
@@ -390,7 +392,19 @@ class ParserFunctions(object):
         if numseg:
             parts = parts[:numseg]
         return "/".join(parts)
-    
+
+    def IFERROR(self, args):
+        errmark = '<strong class="error">'
+        val = args[0]
+        bad=args[1]
+        good=args[2] or val
+        
+        if errmark in val:
+            return bad
+        else:
+            return good
+        
+        
 for x in dir(ParserFunctions):
     if x.startswith("_"):
         continue    
