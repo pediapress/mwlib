7e74f08e153db3de8e28ce9f454c1a7d30cccbb2
ralf@brainbot.com <ralf@brainbot.com>
2008-06-02 06:47:04 +0200

    plexscanner is long gone

--- mwlib/scanner.py
+++ mwlib/scanner.py
@@ -1,6 +1,3 @@
 #! /usr/bin/env python
 
-if 0:
-    from plexscanner import TagToken, EndTagToken, tokenize
-else:
-    from mwscan import TagToken, EndTagToken, tokenize
+from mwscan import TagToken, EndTagToken, tokenize
