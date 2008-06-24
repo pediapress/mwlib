0317a2cfeeb7d395a7372c016c2ef89e304186f3
Johannes Beigel <johannes.beigel@brainbot.com>
2008-06-24 15:55:49 +0200

    fix

--- mwlib/wiki.py
+++ mwlib/wiki.py
@@ -145,7 +145,7 @@ url=
         licenses = []
         for license in self.metabook.licenses:
             if license.get('mw_license_url'):
-                wikitext = utils.fetch_url(license['mw_license_url'], ingore_errors=True)
+                wikitext = utils.fetch_url(license['mw_license_url'], ignore_errors=True)
             elif license.get('mw_rights_text'):
                 wikitext = ''
                 if license.get('mw_rights_icon'):
