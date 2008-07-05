23ad63970957d70c4f0c3bd2cef3da2fde500d6e
Johannes Beigel <johannes.beigel@brainbot.com>
2008-07-05 11:41:22 +0200

    refactored, documented, unittest added

--- mwlib/jobsched.py
+++ mwlib/jobsched.py
@@ -1,4 +1,5 @@
-import atexit
+"""Simplistic threaded job scheduler"""
+
 import Queue
 import threading
 import traceback
@@ -9,66 +10,84 @@ log = Log('mwlib.jobsched')
 
 # ==============================================================================
 
-
 class JobScheduler(object):
+    """Simple threaded job scheduler"""
+    
     def __init__(self, num_threads, do_job):
-        """
+        """Init instance with a number of worker threads and a job callable
+        
         @param num_threads: number of threads to start
         @type num_threads: int
+        
+        @param do_job: callable which gets called with a job ID and job-specific
+            kwargs, which returns some job-specific result, and which performs a
+            job in a separate thread
+        @type do_job: callable with signature do_job(job_id, **kwargs) -> object
         """
         
         self.num_threads = num_threads
         self.do_job = do_job
         self.job_queue = Queue.Queue()
         self.results = {}
+        self.results_lock = threading.RLock()
         self.started = False
     
     def add_job(self, job_id, **kwargs):
+        """Schedule a job to be executed in a separate thread. The job_id and
+        all additional kwargs are passed to the do_job callable given in the
+        constructor.
+        
+        If called for the first time on this instance, the worker threads will
+        be started by this method.
+        
+        @param job_id: unique ID for this job
+        @type job_id: hashable object
+        """
+        
+        def worker():
+            try:
+                while True:
+                    job_id, kwargs = self.job_queue.get()
+                    try:
+                        if job_id == 'die':
+                            break
+                        try:                
+                            result = self.do_job(job_id, **kwargs)
+                        except Exception, exc:
+                            log.ERROR('Error executing job: %s' % exc)
+                            traceback.print_exc()
+                        self.results_lock.acquire()
+                        self.results[job_id] = result
+                        self.results_lock.release()
+                    finally:
+                        self.job_queue.task_done()
+            except:
+                traceback.print_exc()
+        
         if not self.started:
             self.started = True
             for i in range(self.num_threads):
-                JobThread(self).start()
-            atexit.register(self.kill_threads)
+                thread = threading.Thread(target=worker)
+                thread.setDaemon(True)
+                thread.start()
+        
         self.job_queue.put((job_id, kwargs))
     
     def get_results(self):
-        """Wait for all queued jobs to be finished and return result dictionary
+        """Wait for all queued jobs to be finished and return results dictionary
+        mapping job IDs to the results returned by the do_job callable.
+        
+        After this method returns, all threads of this scheduler are killed.
         
         @returns: dictionary containing results
         @rtype: dict
         """
         
-        self.kill_threads()
+        if self.started:
+            for i in range(self.num_threads):
+                self.job_queue.put(('die', None))
+            self.job_queue.join()
+            self.started = False
         return self.results
     
-    def kill_threads(self):
-        if not self.started:
-            return
-        for i in range(self.num_threads):
-            self.job_queue.put(('die', None))
-        self.job_queue.join()
-        self.started = False
-    
 
-# ==============================================================================
-
-
-class JobThread(threading.Thread):
-    def __init__(self, scheduler):
-        super(JobThread, self).__init__()
-        self.scheduler = scheduler
-    
-    def run(self):
-        while True:
-            job_id, kwargs = self.scheduler.job_queue.get()
-            try:
-                if job_id == 'die':
-                    break
-                try:                
-                    self.scheduler.results[job_id] = self.scheduler.do_job(job_id, **kwargs)
-                except Exception, exc:
-                    log.ERROR('Error executing job: %s' % exc)
-                    traceback.print_exc()
-            finally:
-                self.scheduler.job_queue.task_done()
-    
---
+++ tests/test_jobsched.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env py.test
+
+import time
+
+from mwlib.jobsched import JobScheduler
+
+def test_jobsched():
+    def wait(job_id, wait_time=None):
+        print 'start', job_id
+        time.sleep(wait_time)
+        print 'finish', job_id
+        return wait_time
+    
+    sched = JobScheduler(5, wait)
+    for i in range(2): # test reusability
+        n = 20
+        for i in range(n):
+            sched.add_job(i, wait_time=0.01*i)
+        s = time.time()
+        results = sched.get_results()
+        t = time.time() - s
+        print 'get_results took %f secs' % t
+        assert t > 0.01
+        print results
+        for i in range(n):
+            assert results[i] == 0.01*i
