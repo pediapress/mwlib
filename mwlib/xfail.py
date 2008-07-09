67cc0d21398314d39f07d8fac7dccb9548c3d1d8
ralf@brainbot.com <ralf@brainbot.com>
2008-07-09 18:10:01 +0200

    require a ticket for expected failures

--- mwlib/xfail.py
+++ mwlib/xfail.py
@@ -6,17 +6,25 @@ class _stats(object):
 
 stats = _stats()
 stats.xfail = 0
+stats.warnings = []
 
 def report():
     if stats.xfail:
         print stats.xfail, "expected failures"
-
+    print "\n".join(stats.warnings)
+        
 import atexit
 atexit.register(report)
 
 def xfail(fun): # FIXME: what about generators?
     import os
+    doc = fun.__doc__ or ""
+    if 'http://code.pediapress.com/wiki/ticket/' not in doc:
+        stats.warnings.append("expected failure %r does not reference a ticket in it's docstring" % (fun,))
+        
     if 'XFAIL' in os.environ:
+
+
         def doit():
             try:
                 fun()
