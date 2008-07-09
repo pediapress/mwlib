76aa8b563aafe5c3ea0051dab991cdc3f76927ac
ralf@brainbot.com <ralf@brainbot.com>
2008-07-09 17:59:34 +0200

    add conftest.py. skip test_xhtmlwriter

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import py
+import os
+xmllint_not_found = os.system('xmllint --version 2>/dev/null 1>/dev/null')
+
+class Exclude(py.test.collect.Directory):
+    def filefilter(self, path):
+        if path.basename == 'test_xhtmlwriter.py' and xmllint_not_found:
+            print "Skipping", path, "no xmllint found"
+            return False
+        
+        return super(Exclude, self).filefilter(path)
+
+Directory = Exclude
