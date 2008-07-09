78a897414eed1b97c2c37161de472c7a4977cdc0
ralf@brainbot.com <ralf@brainbot.com>
2008-07-09 17:50:34 +0200

    show number of expected failures

--- mwlib/xfail.py
+++ mwlib/xfail.py
@@ -1,6 +1,19 @@
 class UnexpectedSuccess(Exception):
     pass
 
+class _stats(object):
+    pass
+
+stats = _stats()
+stats.xfail = 0
+
+def report():
+    if stats.xfail:
+        print stats.xfail, "expected failures"
+
+import atexit
+atexit.register(report)
+
 def xfail(fun): # FIXME: what about generators?
     import os
     if 'XFAIL' in os.environ:
@@ -8,6 +21,7 @@ def xfail(fun): # FIXME: what about generators?
             try:
                 fun()
             except:
+                stats.xfail += 1
                 return
             raise UnexpectedSuccess('expected %r to fail' % (fun,))
         return doit
