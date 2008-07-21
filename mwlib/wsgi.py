67d5d45342c2bee1342ba4c6c068150bf305eb43
Johannes Beigel <johannes.beigel@brainbot.com>
2008-07-21 17:31:07 +0200

    fix: there is not wsgi.output

--- mwlib/wsgi.py
+++ mwlib/wsgi.py
@@ -88,9 +88,9 @@ class Application(object):
     def __call__(self, env, start_response):
         if os.name == 'nt':
             import msvcrt
-            for fd in (env['wsgi.input'], env['wsgi.output']):
-                if hasattr(fd, 'fileno') and fd.fileno() >= 0:
-                    msvcrt.setmode(fd.fileno(), os.O_BINARY)
+            fd = env['wsgi.input']
+            if hasattr(fd, 'fileno') and fd.fileno() >= 0:
+                msvcrt.setmode(fd.fileno(), os.O_BINARY)
         
         start_time = time.time()
         try:
