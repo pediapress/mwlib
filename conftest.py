8f1f119b03bad7a87fe7bc04c5be54590b713d52
ralf@brainbot.com <ralf@brainbot.com>
2008-10-21 12:33:02 +0200

    skip test_docbookwriter if lxml is not available

--- conftest.py
+++ conftest.py
@@ -4,16 +4,24 @@ xmllint_not_found = os.system('xmllint --version 2>/dev/null 1>/dev/null')
 ploticus_not_found = not os.path.isfile('/usr/bin/ploticus')
 mathrenderer_not_found =  os.system('blahtexml --help 2>/dev/null 1>/dev/null')
 
+try:
+    import lxml
+except ImportError:
+    lxml = None
+    
 class Exclude(py.test.collect.Directory):
     def filefilter(self, path):
         if path.basename == 'test_xhtmlwriter.py' and xmllint_not_found:
-            print "Skipping", path, "no xmllint found"
+            print "Skipping", path, "-- no xmllint found"
             return False
         if path.basename == 'test_timeline.py' and ploticus_not_found:
-            print "Skipping", path, "no ploticus found"
+            print "Skipping", path, "-- no ploticus found"
             return False
         if path.basename == 'test_mathutils.py' and mathrenderer_not_found:
-            print "Skipping", path, 'blahtexml or texvc for math rendering not found'
+            print "Skipping", path, '-- blahtexml or texvc for math rendering not found'
+            return False
+        if not lxml and path.basename=='test_docbookwriter.py':
+            print "Skipping", path, "-- lxml module not found"
             return False
         return super(Exclude, self).filefilter(path)
 
