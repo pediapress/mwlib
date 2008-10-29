70660a2cb1957ef52ce2eba0614436393ba08e16
ralf@brainbot.com <ralf@brainbot.com>
2008-10-29 15:07:04 +0100

    let me exclude some of the tests that need network

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ xmllint_not_found = os.system('xmllint --version 2>/dev/null 1>/dev/null')
 ploticus_not_found = not os.path.isfile('/usr/bin/ploticus')
 mathrenderer_not_found =  os.system('blahtexml --help 2>/dev/null 1>/dev/null')
 
+xnet = 'XNET' in os.environ # eXclude NETwork tests
+
 try:
     import lxml
 except ImportError:
@@ -11,17 +13,22 @@ except ImportError:
     
 class Exclude(py.test.collect.Directory):
     def filefilter(self, path):
-        if path.basename == 'test_xhtmlwriter.py' and xmllint_not_found:
-            print "Skipping", path, "-- no xmllint found"
+        bn = path.basename
+        
+        if bn == 'test_xhtmlwriter.py' and xmllint_not_found:
+            print "Skipping", bn, "-- no xmllint found"
             return False
-        if path.basename == 'test_timeline.py' and ploticus_not_found:
-            print "Skipping", path, "-- no ploticus found"
+        if bn == 'test_timeline.py' and ploticus_not_found:
+            print "Skipping", bn, "-- no ploticus found"
             return False
-        if path.basename == 'test_mathutils.py' and mathrenderer_not_found:
-            print "Skipping", path, '-- blahtexml or texvc for math rendering not found'
+        if bn == 'test_mathutils.py' and mathrenderer_not_found:
+            print "Skipping", bn, '-- blahtexml or texvc for math rendering not found'
             return False
-        if not lxml and path.basename=='test_docbookwriter.py':
-            print "Skipping", path, "-- lxml module not found"
+        if not lxml and bn=='test_docbookwriter.py':
+            print "Skipping", bn, "-- lxml not found"
+            return False
+        if xnet and bn in ('test_mwapidb.py', 'test_netdb_imagedb.py', 'test_zipwiki.py'):
+            print "Skipping", bn, "-- needs network"
             return False
         return super(Exclude, self).filefilter(path)
 
