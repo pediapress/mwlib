81d6968188978e8a49c245b135d009796b80de61
johannes.beigel@brainbot.com <johannes.beigel@brainbot.com>
2008-10-31 14:52:04 +0100

    unify ZipCreator and ThreadedZipCreator. much better progress.

--- mwlib/zipcreator.py
+++ mwlib/zipcreator.py
@@ -32,28 +32,40 @@ class ZipCreator(object):
     
     redirect_rex = re.compile(r'^#redirect:?\s*?\[\[(?P<redirect>.*?)\]\]', re.IGNORECASE)
     
-    def __init__(self, zf, imagesize=None, status=None, num_articles=None):
+    def __init__(self, zf,
+        imagesize=None,
+        num_threads=10,
+        status=None,
+        num_articles=None,
+    ):
         """
         @param zf: ZipFile object
         @type zf: L{zipfile.ZipFile}
         
         @param imagesize: max. size of images
         @type imagesize: int
+        
+        @param num_threads: number of threads for parallel fetchgin
+            (set to 0 to turn threading off)
+        @type num_threads: int
         """
         
         self.zf = zf
         self.imagesize = imagesize
-        self._status = status
-        self.num_articles = num_articles
+        self.status = status
         self.articles = {}
         self.templates = {}
         self.sources = {}
         self.images = {}
+        self.num_articles = num_articles
         self.article_count = 0
-    
-    def status(self, **kwargs):
-        if self._status is not None:
-            self._status(**kwargs)
+        self.image_infos = set()    
+        self.zf_lock = threading.RLock()
+        if num_threads > 0:
+            self.jobsched = jobsched.JobScheduler(num_threads)
+        else:
+            self.jobsched = jobsched.DummyScheduler()
+        self.article_jobs = []
     
     def addObject(self, name, value):
         """Add a file with name and contents value to the ZIP file
@@ -65,7 +77,11 @@ class ZipCreator(object):
         @type value: str
         """
         
-        self.zf.writestr(name.encode('utf-8'), value)
+        self.zf_lock.acquire()
+        try:
+            self.zf.writestr(name.encode('utf-8'), value)
+        finally:
+            self.zf_lock.release()
     
     def addArticle(self, title,
         revision=None,
@@ -86,34 +102,12 @@ class ZipCreator(object):
         @param imagedb: ImageDB to use (optional)
         """
         
-        if title in self.articles:
-            return
-        self.articles[title] = {}
-        
-        self.status(article=title)
-        
-        recorddb = RecordDB(wikidb, self.articles, self.templates, self.sources)
-        raw = recorddb.getRawArticle(title, revision=revision)
-        if raw is None:
-            log.warn('Could not get article %r' % title)
-            return
-        mo = self.redirect_rex.search(raw)
-        if mo:
-            raw = recorddb.getRawArticle(mo.group('redirect'))
-            if raw is None:
-                log.warn('Could not get redirected article %r (from %r)' % (
-                    mo.group('redirect'), title
-                ))
-                return
-        self.parseArticle(title,
-            revision=revision,
-            raw=raw,
-            wikidb=wikidb,
-            imagedb=imagedb,
-        )
-        self.article_count += 1
-        if self.num_articles:
-            self.status(progress=self.article_count*100//self.num_articles)
+        self.article_jobs.append({
+            'title': title,
+            'revision': revision,
+            'wikidb': wikidb,
+            'imagedb': imagedb,
+        })
     
     def parseArticle(self, title,
         revision=None,
@@ -148,140 +142,25 @@ class ZipCreator(object):
             return
         for node in parse_tree.allchildren():
             if isinstance(node, parser.ImageLink):
-                self.addImage(node.target, imagedb=imagedb, wikidb=wikidb)
+                self.image_infos.add((node.target, imagedb, wikidb))
             elif isinstance(node, parser.TagNode) and node.caption == 'imagemap':
                 imagemap = getattr(node, 'imagemap', None)
                 if imagemap is not None:
                     imagelink = getattr(imagemap, 'imagelink', None)
                     if imagelink is not None:
-                        self.addImage(imagelink.target, imagedb=imagedb, wikidb=wikidb)
-
-    
-    def addImage(self, name, imagedb=None, wikidb=None):
-        """Add image with given name to the ZIP file
-        
-        @param name: image name
-        @type name: unicode
-        
-        @param imagedb: ImageDB to use
-        """
-        
-        if name in self.images:
-            return
-        self.images[name] = {}
-        
-        path = imagedb.getDiskPath(name, size=self.imagesize)
-        if path is None:
-            log.warn('Could not get image %r' % name)
-            return
-        zipname = u"images/%s" % name.replace("'", '-')
-        self.zf.write(path, zipname.encode("utf-8"))
-        self.images[name]['url'] = imagedb.getURL(name, size=self.imagesize)
-        descriptionurl = imagedb.getDescriptionURL(name)
-        if descriptionurl:
-            self.images[name]['descriptionurl'] = descriptionurl
-        templates = imagedb.getImageTemplates(name, wikidb=wikidb)
-        if templates:
-            self.images[name]['templates'] = templates
+                        self.image_infos.add((imagelink.target, imagedb, wikidb))
     
     def join(self):
         """Finish ZIP file by writing the actual content"""
         
-        self.addObject('content.json', json.dumps(dict(
-            articles=self.articles,
-            templates=self.templates,
-            sources=self.sources,
-            images=self.images,
-        )))
-    
-
-# ==============================================================================
-
-
-class ThreadedZipCreator(ZipCreator):
-    """Create ZIP files usable as WikiDB
-    
-    See docs/zipfile.txt
-    """
-    
-    redirect_rex = re.compile(r'^#redirect:?\s*?\[\[(?P<redirect>.*?)\]\]', re.IGNORECASE)
-    
-    def __init__(self, zf,
-        imagesize=None,
-        num_threads=10,
-        status=None,
-        num_articles=None,
-    ):
-        """
-        @param zf: ZipFile object
-        @type zf: L{zipfile.ZipFile}
-        
-        @param imagesize: max. size of images
-        @type imagesize: int
-        
-        @param num_threads: number of threads for parallel fetchgin
-            (set to 0 to turn threading off)
-        @type num_threads: int
-        """
-        
-        super(ThreadedZipCreator, self).__init__(zf,
-            imagesize=imagesize,
-            status=status,
-            num_articles=num_articles,
-        )
-        self.zf_lock = threading.RLock()
-        if num_threads > 0:
-            self.jobsched = jobsched.JobScheduler(num_threads)
+        if self.status:
+            self.status(status=u'fetching articles')
+            self.fetcharticle_status = self.status.getSubRange(0, 20)
+            self.fetchtemplate_status = self.status.getSubRange(21, 40)
+            self.parse_status = self.status.getSubRange(41, 60)
+            self.fetchimages_status = self.status.getSubRange(61, 100)
         else:
-            self.jobsched = jobsched.DummyScheduler()
-        self.article_jobs = []
-    
-    def addObject(self, name, value):
-        """Add a file with name and contents value to the ZIP file
-        
-        @param name: name for entry in ZIP file, will be UTF-8 encoded
-        @type name: unicode
-        
-        @param value: content for ZIP file entry
-        @type value: str
-        """
-        
-        self.zf_lock.acquire()
-        try:
-            super(ThreadedZipCreator, self).addObject(name, value)
-        finally:
-            self.zf_lock.release()
-    
-    def addArticle(self, title,
-        revision=None,
-        wikidb=None,
-        imagedb=None,
-    ):
-        """Add article with given title and revision to ZIP file. This will add
-        all referenced templates and images, too.
-        
-        @param title: article title
-        @type title: unicode
-        
-        @param revision: article revision (optional)
-        @type revision: int
-        
-        @param wikidb: WikiDB to use
-        
-        @param imagedb: ImageDB to use (optional)
-        """
-        
-        self.article_jobs.append({
-            'title': title,
-            'revision': revision,
-            'wikidb': wikidb,
-            'imagedb': imagedb,
-        })
-    
-    def join(self):
-        """Finish ZIP file by writing the actual content"""
-        
-        self.status(status=u'fetching articles')
+            self.fetcharticle_status = self.fetchtemplate_status = self.parse_status = self.fetchimages_status = None
         for info in self.article_jobs:
             self.fetchArticle(
                 title=info['title'],
@@ -289,9 +168,8 @@ class ThreadedZipCreator(ZipCreator):
                 wikidb=info['wikidb'],
             )
         self.jobsched.join()
-        self.status(progress=33)
-        
-        self.status(status=u'fetching templates')
+        if self.status:
+            self.status(status=u'fetching templates')
         templates = set()
         for info in self.article_jobs:
             try:
@@ -301,14 +179,17 @@ class ThreadedZipCreator(ZipCreator):
             
             for name in expander.get_templates(raw):
                 templates.add((name, info['wikidb']))
-                              
+                            
+        self.num_templates = len(templates)
+        self.template_count = 0 
         for title, wikidb in templates:
             self.fetchTemplate(title, wikidb)
         self.jobsched.join()
-        self.status(progress=50)
         
-        self.status(status=u'fetching images')
-        for info in self.article_jobs:
+        if self.status:
+            self.status(status=u'parsing articles')
+        n = len(self.article_jobs)
+        for i, info in enumerate(self.article_jobs):
             try:
                 raw = self.articles[info['title']]['content']
             except KeyError:
@@ -320,6 +201,15 @@ class ThreadedZipCreator(ZipCreator):
                 wikidb=info['wikidb'],
                 imagedb=info['imagedb'],
             )
+            if self.parse_status:
+                self.parse_status(progress=i*100/n)
+        
+        if self.status:
+            self.status(status=u'fetching images')
+        self.num_images = len(self.image_infos)
+        self.image_count = 0
+        for i in self.image_infos:
+            self.addImage(*i)
         self.jobsched.join()
         
         self.addObject('content.json', json.dumps(dict(
@@ -335,22 +225,29 @@ class ThreadedZipCreator(ZipCreator):
             raw = recorddb.getRawArticle(title, revision=revision)
             if raw is None:
                 log.warn('Could not get article %r' % title)
-                return
-            mo = self.redirect_rex.search(raw)
-            if mo:
-                raw = recorddb.getRawArticle(mo.group('redirect'))
-                if raw is None:
-                    log.warn('Could not get redirected article %r (from %r)' % (
-                        mo.group('redirect'), title
-                    ))
-                    return
+            else:
+                mo = self.redirect_rex.search(raw)
+                if mo:
+                    raw = recorddb.getRawArticle(mo.group('redirect'))
+                    if raw is None:
+                        log.warn('Could not get redirected article %r (from %r)' % (
+                            mo.group('redirect'), title
+                        ))
+            self.article_count += 1
+            if self.fetcharticle_status:
+                self.fetcharticle_status(progress=self.article_count*100/self.num_articles)
         
         self.jobsched.add_job(title, fetch_article_job) # FIXME: title is not unique
     
     def fetchTemplate(self, name, wikidb):
         def get_template_job(name):
             recorddb = RecordDB(wikidb, self.articles, self.templates, self.sources)
-            recorddb.getTemplate(name)
+            try:
+                recorddb.getTemplate(name)
+            finally:
+                self.template_count += 1
+                if self.fetchtemplate_status:
+                    self.fetchtemplate_status(progress=self.template_count*100/self.num_templates)
         
         self.jobsched.add_job(name, get_template_job)
     
@@ -385,6 +282,9 @@ class ThreadedZipCreator(ZipCreator):
             templates = imagedb.getImageTemplates(name, wikidb=wikidb)
             if templates:
                 self.images[name]['templates'] = templates
+            if self.fetchimages_status:
+                self.image_count += 1
+                self.fetchimages_status(progress=self.image_count*100/self.num_images)
         
         self.jobsched.add_job(name, fetch_image_job)
     
@@ -410,30 +310,12 @@ def make_zip_file(output, env,
     try:
         articles = metabook.get_item_list(env.metabook, filter_type='article')
         
-        if num_threads > 0:
-            z = ThreadedZipCreator(zf,
-                imagesize=imagesize,
-                num_threads=num_threads,
-                status=status,
-                num_articles=len(articles),
-            )
-        else:
-            z = ZipCreator(zf,
-                imagesize=imagesize,
-                status=status,
-                num_articles=len(articles),
-            )
-        
-        # if articles:
-        #     class IncProgress(object):
-        #         inc = 100./len(articles)
-        #         p = 0
-        #         def __call__(self, title):
-        #             self.p += self.inc
-        #             status(progress=int(self.p), article=title)
-        #     inc_progress = IncProgress()
-        # else:
-        #     inc_progress = None
+        z = ZipCreator(zf,
+            imagesize=imagesize,
+            num_threads=num_threads,
+            status=status,
+            num_articles=len(articles),
+        )
         
         for item in articles:
             d = mwapidb.parse_article_url(item['title'].encode('utf-8'))
