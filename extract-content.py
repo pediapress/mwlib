6a5c6ce1bc4c464d44774c63be1fb9d50d1b3773
ralf@brainbot.com <ralf@brainbot.com>
2009-01-26 14:08:12 +0100

    extract articles

--- extract-content.py
+++ extract-content.py
@@ -5,9 +5,18 @@ try:
 except ImportError:
     import simplejson as json
 
-if __name__=='__main__':
-    d=json.load(open("content.json"))
-    for name, t in d['templates'].items():
+def exdict(d, name):
+    for name, t in d.items():
         header = u" Template:%s" % (name,)
         print header.encode('utf-8')
         print t['content'].encode("utf-8")
+
+
+def main():
+    d=json.load(open("content.json"))
+    exdict(d['templates'], 'Template')
+    exdict(d['articles'], 'Article')
+    
+if __name__=='__main__':
+    main()
+    
