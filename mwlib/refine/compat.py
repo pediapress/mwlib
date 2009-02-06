8c79648851100b6af4527acc17d7f8c0a0bd2c81
ralf@brainbot.com <ralf@brainbot.com>
2009-02-06 16:15:55 +0100

    workaround for advtree, which can't handle blists.

--- mwlib/refine/compat.py
+++ mwlib/refine/compat.py
@@ -23,6 +23,8 @@ def _change_classes(node):
             node.caption=node.text
         if node.children is None:
             node.children = []
+        else:
+            node.children = list(node.children) # advtree can't handle the blist
         if node.vlist is None:
             node.vlist = {}
         if node.type==T.t_complex_tag:
