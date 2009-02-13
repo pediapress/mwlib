a2042c37e0c3a110b38fe6590a0b310ecedfeefa
ralf@brainbot.com <ralf@brainbot.com>
2009-02-13 12:13:24 +0100

    fix namespace handling.

--- extract-content.py
+++ extract-content.py
@@ -1,26 +1,30 @@
 #! /usr/bin/env python
 
 import sys
+from mwlib import namespace
 
 try:
     import json
 except ImportError:
     import simplejson as json
 
-def exdict(d, ns):
+def exdict(d, default_ns):
+    
     for name, t in d.items():
+        ns, partial, full = namespace.splitname(name, default_ns)
+        
         if t['content'] is None:
-            sys.stderr.write("no such %s:%s\n" % (ns, name))
+            sys.stderr.write("no such %r\n" % full)
             continue
-        header = u" %s:%s" % (ns, name)
+        header = u" %s" % full
         print header.encode('utf-8')
         print t['content'].encode("utf-8")
 
 
 def main():
     d=json.load(open("content.json"))
-    exdict(d['templates'], 'Template')
-    exdict(d['articles'], 'Article')
+    exdict(d['templates'], namespace.NS_TEMPLATE)
+    exdict(d['articles'], namespace.NS_MAIN)
     
 if __name__=='__main__':
     main()
