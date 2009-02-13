8686e4c193efe6ab83493057bb0106ebc017fcd7
johannes.beigel@brainbot.com <johannes.beigel@brainbot.com>
2009-02-13 15:48:50 +0100

    do *not* use getTemplatesForArticles() for now...

--- mwlib/zipcreator.py
+++ mwlib/zipcreator.py
@@ -183,16 +183,13 @@ class ZipCreator(object):
             self.status(status=u'fetching templates', article='')
         templates = set()
         for info in self.article_jobs:
-            if hasattr(info['wikidb'], 'getTemplatesForArticle'):
-                self.templates.update(info['wikidb'].getTemplatesForArticle(info['title'], revision=info.get('revision')))
-            else:
-                try:
-                    raw = self.articles[info['title']]['content']
-                except KeyError:
-                    continue
+            try:
+                raw = self.articles[info['title']]['content']
+            except KeyError:
+                continue
 
-                for name in expander.get_templates(raw, info['title']):
-                    templates.add((name, info['wikidb']))
+            for name in expander.get_templates(raw, info['title']):
+                templates.add((name, info['wikidb']))
                             
         self.num_templates = len(templates)
         self.template_count = 0 
