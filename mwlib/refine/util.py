c38402063a7e172405533ef1506bb75519d71b26
ralf@brainbot.com <ralf@brainbot.com>
2009-03-05 15:01:34 +0100

    move ImageMod to mwlib.refine.util

--- mwlib/refine/util.py
+++ mwlib/refine/util.py
@@ -34,3 +34,62 @@ def parseParams(s):
         else:
             r[name] = maybeInt(value)
     return r
+
+
+
+class ImageMod(object):
+    default_magicwords = [
+        {u'aliases': [u'thumbnail', u'thumb'], u'case-sensitive': u'', u'name': u'img_thumbnail'},
+        {u'aliases': [u'thumbnail=$1', u'thumb=$1'], u'case-sensitive': u'', u'name': u'img_manualthumb'},
+        {u'aliases': [u'right'], u'case-sensitive': u'', u'name': u'img_right'},
+        {u'aliases': [u'left'], u'case-sensitive': u'', u'name': u'img_left'},
+        {u'aliases': [u'none'], u'case-sensitive': u'', u'name': u'img_none'},
+        {u'aliases': [u'$1px'], u'case-sensitive': u'', u'name': u'img_width'},
+        {u'aliases': [u'center', u'centre'], u'case-sensitive': u'', u'name': u'img_center'},
+        {u'aliases': [u'framed', u'enframed', u'frame'], u'case-sensitive': u'', u'name': u'img_framed'},
+        {u'aliases': [u'frameless'], u'case-sensitive': u'', u'name': u'img_frameless'},
+        {u'aliases': [u'page=$1', u'page $1'], u'case-sensitive': u'', u'name': u'img_page'},
+        {u'aliases': [u'upright', u'upright=$1', u'upright $1'], u'case-sensitive': u'', u'name': u'img_upright'},
+        {u'aliases': [u'border'], u'case-sensitive': u'', u'name': u'img_border'},
+        {u'aliases': [u'baseline'], u'case-sensitive': u'', u'name': u'img_baseline'},
+        {u'aliases': [u'sub'], u'case-sensitive': u'', u'name': u'img_sub'},
+        {u'aliases': [u'super', u'sup'], u'case-sensitive': u'', u'name': u'img_super'},
+        {u'aliases': [u'top'], u'case-sensitive': u'', u'name': u'img_top'},
+        {u'aliases': [u'text-top'], u'case-sensitive': u'', u'name': u'img_text_top'},
+        {u'aliases': [u'middle'], u'case-sensitive': u'', u'name': u'img_middle'},
+        {u'aliases': [u'bottom'], u'case-sensitive': u'', u'name': u'img_bottom'},
+        {u'aliases': [u'text-bottom'], u'case-sensitive': u'', u'name': u'img_text_bottom'},
+        {u'aliases': [u'link=$1'], u'case-sensitive': u'', u'name': u'img_link'},
+        {u'aliases': [u'alt=$1'], u'case-sensitive': u'', u'name': u'img_alt'},
+        ]
+
+    def __init__(self, magicwords):        
+        self.alias_map = {}
+        self.initAliasMap(self.default_magicwords)
+        self.initAliasMap(magicwords)
+
+    def initAliasMap(self, magicwords):
+        for m in magicwords:            
+            if not m['name'].startswith('img_'):
+                continue
+            name = m['name']
+            aliases = m['aliases']
+            aliases_regexp = '|'.join(['^(%s)$' % re.escape(a) for a in aliases])
+            if name == 'img_upright':
+                aliases_regexp = aliases_regexp.replace('\\$1', '\\s*([0-9.]+)\\s*')
+            elif name == 'img_width':
+                aliases_regexp = aliases_regexp.replace('\\$1', '\\s*([0-9x]+)\\s*')
+            elif name in ['img_alt', 'img_link']:
+                aliases_regexp = aliases_regexp.replace('\\$1', '(.*)')
+            self.alias_map[name] = aliases_regexp
+
+    def parse(self, mod):
+        mod = mod.lower().strip()
+        for mod_type, mod_reg in self.alias_map.items():
+            rx = re.compile(mod_reg, re.IGNORECASE)
+            mo = rx.match(mod)
+            if mo:
+                for match in  mo.groups()[::-1]:
+                    if match:
+                        return (mod_type, match)
+        return (None, None)
