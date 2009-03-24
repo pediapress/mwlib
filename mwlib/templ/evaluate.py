9efe505e9e85b2956d0a6dc5d3e74485c3d35f5e
ralf@brainbot.com <ralf@brainbot.com>
2009-03-24 11:48:47 +0100

    move uniquifier.

--- mwlib/templ/evaluate.py
+++ mwlib/templ/evaluate.py
@@ -4,7 +4,7 @@
 
 import re
 from mwlib.templ import magics, log, DEBUG, parser, mwlocals
-
+from mwlib.uniq import Uniquifier
 
 class TemplateRecursion(Exception): pass
 
@@ -181,48 +181,6 @@ def _insert_implicit_newlines(res, maybe_newline=maybe_newline):
                     res[i] = '\n'
     del res[-2:]
 
-class Uniquifier(object):
-    random_string = None 
-    def __init__(self):
-        self.uniq2repl = {}
-        
-    def get_uniq(self, repl):
-        r = self.get_random_string()
-        count = len(self.uniq2repl)
-        retval = "\x7fUNIQ-%s-%s-QINU\x7f" % (count, r)
-        self.uniq2repl[retval] = repl
-        return retval
-    
-        
-    def get_random_string(self):
-        if self.random_string is None:
-            import binascii
-            r=open("/dev/urandom").read(8)
-            self.__class__.random_string = binascii.hexlify(r)
-        return self.random_string
-
-    def _repl_from_uniq(self, mo):
-        u = mo.group(0)
-        return self.uniq2repl.get(u, u)
-
-    def replace_uniq(self, txt):
-        rx=re.compile("\x7fUNIQ-\\d+-[a-f0-9]+-QINU\x7f")
-        txt = rx.sub(self._repl_from_uniq, txt)
-        return txt
-    
-    def _repl_to_uniq(self, mo):
-        return self.get_uniq(mo.group(0))
-    
-    def replace_tags(self, txt):
-        rx=re.compile("""(?:<nowiki>.*?</nowiki>)          # nowiki
-|(?:<math>.*?</math>)
-|(?:<imagemap[^<>]*>.*?</imagemap>)
-|(?:<gallery[^<>]*>.*?</gallery>)
-|(?:<ref[^<>]*/?>)
-|(?:<source[^<>]*>.*?</source>)
-|(?:<pre.*?>.*?</pre>)""", re.VERBOSE | re.DOTALL | re.IGNORECASE)
-        newtxt = rx.sub(self._repl_to_uniq, txt)
-        return newtxt
     
 class Expander(object):
     def __init__(self, txt, pagename="", wikidb=None, recursion_limit=100):
--- mwlib/uniq.py
+++ mwlib/uniq.py
@@ -9,10 +9,10 @@ class Uniquifier(object):
     def __init__(self):
         self.uniq2repl = {}
         
-    def get_uniq(self, repl):
+    def get_uniq(self, repl, name):
         r = self.get_random_string()
         count = len(self.uniq2repl)
-        retval = "\x7fUNIQ-%s-%s-QINU\x7f" % (count, r)
+        retval = "\x7fUNIQ-%s-%s-%s-QINU\x7f" % (name, count, r)
         self.uniq2repl[retval] = repl
         return retval
     
@@ -25,24 +25,30 @@ class Uniquifier(object):
         return self.random_string
 
     def _repl_from_uniq(self, mo):
+        
         u = mo.group(0)
         return self.uniq2repl.get(u, u)
 
     def replace_uniq(self, txt):
-        rx=re.compile("\x7fUNIQ-\\d+-[a-f0-9]+-QINU\x7f")
+        rx=re.compile("\x7fUNIQ-[a-z]+-\\d+-[a-f0-9]+-QINU\x7f")
         txt = rx.sub(self._repl_from_uniq, txt)
         return txt
     
     def _repl_to_uniq(self, mo):
-        return self.get_uniq(mo.group(0))
+        for name, s in mo.groupdict().items():
+            if s:
+                return self.get_uniq(s, name)
+        assert 0
+        # print "GROUPS:", mo.groupdict()
+        # return self.get_uniq(mo.group(0))
     
     def replace_tags(self, txt):
-        rx=re.compile("""(?:<nowiki>.*?</nowiki>)          # nowiki
-|(?:<math>.*?</math>)
-|(?:<imagemap[^<>]*>.*?</imagemap>)
-|(?:<gallery[^<>]*>.*?</gallery>)
-|(?:<ref[^<>]*/?>)
-|(?:<source[^<>]*>.*?</source>)
-|(?:<pre.*?>.*?</pre>)""", re.VERBOSE | re.DOTALL | re.IGNORECASE)
+        rx=re.compile("""(?P<nowiki><nowiki>.*?</nowiki>)          # nowiki
+|(?P<math><math>.*?</math>)
+|(?P<imagemap><imagemap[^<>]*>.*?</imagemap>)
+|(?P<gallery><gallery[^<>]*>.*?</gallery>)
+|(?P<ref><ref[^<>]*/?>)
+|(?P<source><source[^<>]*>.*?</source>)
+|(?P<pre><pre.*?>.*?</pre>)""", re.VERBOSE | re.DOTALL | re.IGNORECASE)
         newtxt = rx.sub(self._repl_to_uniq, txt)
         return newtxt
