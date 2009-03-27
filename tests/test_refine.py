2be7ec55f9a4652ac1567a5696531be7359eb66c
ralf@brainbot.com <ralf@brainbot.com>
2009-03-27 16:01:21 +0100

    add test. needs more work

--- tests/test_refine.py
+++ tests/test_refine.py
@@ -1,16 +1,17 @@
 #! /usr/bin/env py.test
 
-import mwlib.refine.core as refine
+from mwlib.refine import core
 
-tokenize = refine.tokenize
-show = refine.show
-T = refine.T
+tokenize = core.tokenize
+show = core.show
+T = core.T
+parse_txt = core.parse_txt
 
 
 
 def test_parse_row_missing_beginrow():
     tokens = tokenize("<td>implicit row starts here</td><td>cell2</td>")
-    refine.parse_table_rows(tokens, [])
+    core.parse_table_rows(tokens, [])
     show(tokens)
     assert len(tokens)==1
     assert tokens[0].type == T.t_complex_table_row
@@ -18,15 +19,15 @@ def test_parse_row_missing_beginrow():
     
 
 def test_parse_table_cells_missing_close():
-    tokens = refine.tokenize("<td>bla")
-    refine.parse_table_cells(tokens, [])
+    tokens = core.tokenize("<td>bla")
+    core.parse_table_cells(tokens, [])
     show(tokens)
     assert tokens[0].type==T.t_complex_table_cell, "expected a complex table cell"
 
 
 def test_parse_table_cells_closed_by_next_cell():
-    tokens = refine.tokenize("<td>foo<td>bar")
-    refine.parse_table_cells(tokens, [])
+    tokens = core.tokenize("<td>foo<td>bar")
+    core.parse_table_cells(tokens, [])
     show(tokens)
     assert tokens[0].type==T.t_complex_table_cell
     assert tokens[1].type==T.t_complex_table_cell
@@ -38,7 +39,7 @@ def test_parse_table_cells_pipe():
     tokens = tokenize("{|\n|cell0||cell1||cell2\n|}")[2:-2]
     print "BEFORE:"
     show(tokens)
-    refine.parse_table_cells(tokens, [])
+    core.parse_table_cells(tokens, [])
     print "AFTER"
     show(tokens)
     assert len(tokens)==3
@@ -57,7 +58,7 @@ def test_parse_cell_modifier():
     
     print "BEFORE:"
     show(tokens)
-    refine.parse_table_cells(tokens, [])
+    core.parse_table_cells(tokens, [])
     print "AFTER"
     show(tokens)
     assert tokens[0].type == T.t_complex_table_cell
@@ -69,7 +70,7 @@ def test_parse_cell_modifier():
 def test_parse_table_refined():
     tokens = tokenize("<table><tr><td>cell1</td></tr></table>")
     refined = []
-    refine.parse_tables(tokens, refined)
+    core.parse_tables(tokens, refined)
     print "REFINED:", refined
     assert len(refined)==2
     
@@ -81,7 +82,7 @@ def test_parse_table_modifier():
     
     print "BEFORE:"
     show(tokens)
-    refine.parse_tables(tokens, [])
+    core.parse_tables(tokens, [])
     
     print "AFTER:"
     show(tokens)
@@ -99,7 +100,7 @@ def test_parse_table_row_modifier():
     
     print "BEFORE:"
     show(tokens)
-    refine.parse_table_rows(tokens, [])
+    core.parse_table_rows(tokens, [])
     
     print "AFTER:"
     show(tokens)
@@ -110,7 +111,7 @@ def test_parse_table_row_modifier():
 def test_parse_link():
     tokens = tokenize("[[link0]][[link2]]")
     refined = []
-    refine.parse_links(tokens, [])
+    core.parse_links(tokens, [])
     show(tokens)
     assert len(tokens)==2
     assert tokens[0].type == T.t_complex_link
@@ -118,19 +119,19 @@ def test_parse_link():
 
 def test_no_row_modifier():    
     s="{|\n|foo||bar\n|}"
-    r=refine.parse_txt(s)
-    refine.show(r)
-    cells = list(refine.walknode(r, lambda x: x.type==refine.T.t_complex_table_cell))
+    r=core.parse_txt(s)
+    core.show(r)
+    cells = list(core.walknode(r, lambda x: x.type==core.T.t_complex_table_cell))
     print "CELLS:", cells
     assert len(cells)==2, "expected 2 cells"
     
 def test_parse_para_vs_preformatted():
     s=' foo\n\nbar\n'
-    r=refine.parse_txt(s)
-    refine.show(r)
-    pre = list(refine.walknode(r, lambda x: x.type==refine.T.t_complex_preformatted))[0]
-    refine.show(pre)
-    textnodes = list(refine.walknode(pre, lambda x: x.type==refine.T.t_text))
+    r=core.parse_txt(s)
+    core.show(r)
+    pre = list(core.walknode(r, lambda x: x.type==core.T.t_complex_preformatted))[0]
+    core.show(pre)
+    textnodes = list(core.walknode(pre, lambda x: x.type==core.T.t_text))
     txt=''.join([x.text for x in textnodes])
     assert u'bar' not in txt
                
@@ -138,9 +139,9 @@ def test_duplicate_nesting():
     s=u"""<b>
 [[foo|bar]] between
 </b>"""
-    r = refine.parse_txt(s)
-    bolds = list(refine.walknode(r, lambda x: x.tagname=="b"))
-    refine.show(bolds)
+    r = core.parse_txt(s)
+    bolds = list(core.walknode(r, lambda x: x.tagname=="b"))
+    core.show(bolds)
     
     for x in bolds:
         for y in x.children or []:
@@ -148,9 +149,9 @@ def test_duplicate_nesting():
 
 def test_ref_no_newline():
     s=u"""<ref>* no item</ref>"""
-    r = refine.parse_txt(s)
-    refine.show(r)
-    linodes = list(refine.walknode(r, lambda x: x.tagname=="li"))
+    r = core.parse_txt(s)
+    core.show(r)
+    linodes = list(core.walknode(r, lambda x: x.tagname=="li"))
     assert not linodes
 
 def test_tab_table():
@@ -161,8 +162,8 @@ def test_tab_table():
 | cell2
 \t|}after
 """
-    r = refine.parse_txt(s)
-    refine.show(r)
+    r = core.parse_txt(s)
+    core.show(r)
     tables = []
     
     def allowed(node):
@@ -173,6 +174,17 @@ def test_tab_table():
     nodes = [x for x in r if allowed(x)]
     assert nodes, "bad  or no table"
 
-    cells = refine.walknodel(r, lambda x: x.type==T.t_complex_table_cell)
+    cells = core.walknodel(r, lambda x: x.type==T.t_complex_table_cell)
     assert len(cells)==2, "expected two cells"
-    
+
+def test_parse_ul_not_preformatted():
+    """http://code.pediapress.com/wiki/ticket/554"""
+    s="""
+<ul>
+   <li>bla blub
+   <li>bla bla
+ </ul>
+"""
+    r=parse_txt(s)
+    core.show(r)
+    assert 0
