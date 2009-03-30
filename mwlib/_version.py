f111ce83f800a0e64f600e441cc8c7d56b7d3bfc
ralf@brainbot.com <ralf@brainbot.com>
2009-03-30 14:42:31 +0200

    up version to 0.10.1.dev

--- mwlib/_version.py
+++ mwlib/_version.py
@@ -5,5 +5,5 @@ class _Version(tuple):
     def __str__(self):
         return '.'.join([str(x) for x in self])
 
-version = _Version((0,9,50, 'dev'))
+version = _Version((0,10,1, 'dev'))
 del _Version
