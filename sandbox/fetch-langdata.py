a9b9eba6e1db7d10b5ff237c29bacfa5b6e50c6b
ralf@brainbot.com <ralf@brainbot.com>
2009-04-03 16:00:09 +0200

    ask for interwikimap

--- sandbox/fetch-langdata.py
+++ sandbox/fetch-langdata.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python
-
+import sys
 from mwlib import lang, mwapidb
 
 def main():
-    languages = list(lang.languages)
-    languages.sort()
+    languages = sys.argv[1:]
+    if not languages:
+        languages = list(lang.languages)
+        languages.sort()
     langdata = {}
     for l in languages:
         w=mwapidb.APIHelper("http://%s.wikipedia.org/w" % l)
-        res = w.query(meta='siteinfo', siprop='general|namespaces|namespacealiases|magicwords')
+        res = w.query(meta='siteinfo', siprop='general|namespaces|namespacealiases|magicwords|interwikimap')
         if res is not None:
             langdata[l] = res["query"]
     import pprint
