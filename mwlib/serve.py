047c517d4abe947493b9a51c6f93f0f794a9ba0f
johannes.beigel@brainbot.com <johannes.beigel@brainbot.com>
2009-04-07 09:47:14 +0200

    use atime instead of mtime

--- mwlib/serve.py
+++ mwlib/serve.py
@@ -557,7 +557,7 @@ def get_collection_dirs(cache_dir):
                 yield os.path.join(dirpath, d)
 
 def purge_cache(max_age, cache_dir):
-    """Remove all subdirectories of cache_dir whose mtime is before now-max_age
+    """Remove all subdirectories of cache_dir whose atime is before now-max_age
     
     @param max_age: max age of directories in seconds
     @type max_age: int
@@ -568,7 +568,7 @@ def purge_cache(max_age, cache_dir):
     
     now = time.time()
     for path in get_collection_dirs(cache_dir):
-        if now - os.stat(path).st_mtime < max_age:
+        if now - os.stat(path).st_atime < max_age:
             continue
         try:
             log.info('removing directory %r' % path)
