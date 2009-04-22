60ded40710792fbdc7122adb5867c0965169b2ed
ralf@brainbot.com <ralf@brainbot.com>
2009-04-22 13:01:05 +0200

    removed

--- mwlib/siteinfo/__init__.py
+++
@@ -1,2 +0,0 @@
-from mwlib.siteinfo._registry import register, lang2siteinfo
-from mwlib.siteinfo import siteinfo_en
--- mwlib/siteinfo/_registry.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) PediaPress GmbH
-# See README.txt for additional licensing information.
-
-lang2siteinfo = {}
-
-def register(name, s):
-    lang2siteinfo[name] = s
