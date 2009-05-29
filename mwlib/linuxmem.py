c48674e80fc873b723f19cb42450172bcf0a5259
ralf@brainbot.com <ralf@brainbot.com>
2009-05-29 13:21:26 +0200

    add linuxmem for debugging

---
+++ mwlib/linuxmem.py
@@ -0,0 +1,48 @@
+_scale = dict(kB=1024.0,  KB=1024.0,
+              mB=1024.0*1024.0, MB=1024.0*1024.0)
+
+# convert byte o MB
+for k, v in _scale.items():
+    _scale[k] = v / (1024.0*1024.0)
+    
+              
+def _readproc(key):
+    '''Private.
+    '''
+    try:
+        v = open("/proc/self/status").read()
+    except:
+        return 0.0  # non-Linux?
+    
+     # get key line e.g. 'VmRSS:  9999  kB\n ...'
+    i = v.index(key)
+    v = v[i:].split(None, 3)  # whitespace
+    if len(v) < 3:
+        return 0.0  # invalid format?
+     # convert Vm value to bytes
+    return float(v[1]) * _scale[v[2]] 
+
+
+def memory():
+    '''Return memory usage in MB.
+    '''
+    return _readproc('VmSize:')
+
+
+def resident():
+    '''Return resident memory usage in MB.
+    '''
+    return _readproc('VmRSS:')
+
+
+def stacksize():
+    '''Return stack size in MB.
+    '''
+    return _readproc('VmStk:')
+
+def report():
+    print "-"*40
+    print "MEM:", memory()
+    print "RES:", resident()
+    print "STACK:", stacksize()
+    print "-"*40
