60dd98241fe5efb1d1f6af4eae92aad2e3d40017
volker@brainbot.com <volker@brainbot.com>
2009-07-13 15:38:53 +0200

    fix and test for splitTableLists

--- mwlib/treecleaner.py
+++ mwlib/treecleaner.py
@@ -1176,7 +1176,7 @@ class TreeCleaner(object):
             max_items = 0
             all_items = []
             for cell in node.children:
-                items = [ item for item in cell.getChildNodesByClass(Item) if not item.getParentNodesByClass(ItemList)]
+                items = [ item for item in cell.getChildNodesByClass(Item) if len(item.getParentNodesByClass(ItemList)) < 2]
                 max_items = max(max_items, len(items))
                 all_items.append(items)
                 for item in cell:
