57681244ed283dd13a00d6c44f0bc2ae6d6d037b
ralf@brainbot.com <ralf@brainbot.com>
2009-08-13 16:17:18 +0200

    test that color values with hash marks work in parseParams

--- tests/test_parse_params.py
+++ tests/test_parse_params.py
@@ -6,4 +6,9 @@ from mwlib.refine.util import parseParams
 def test_display_none():
     r=parseParams("class=geo style=display:none")
     assert r=={'class': 'geo', 'style': {'display': 'none'}}
- 
+
+def test_bgcolor_hashmark_no_doublequote():
+    """http://code.pediapress.com/wiki/ticket/654"""
+    r=parseParams("bgcolor=#ffffff")
+    assert r==dict(bgcolor="#ffffff")
+    
