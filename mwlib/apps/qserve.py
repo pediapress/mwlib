e183e365d48657d2b01bff02d61b55e2ced4ac6e
Ralf Schmitt <ralf@systemexit.de>
2009-11-02 10:51:16 +0100

    option parsing

--- mwlib/apps/qserve.py
+++ mwlib/apps/qserve.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 
+import getopt # yes, getopt!
+import sys
+
 from mwlib.async import jobs
     
 class db(object):
@@ -43,17 +46,46 @@ class qplugin:
         
         
         
-    
+def usage():
+    print "mw-qserve [-p PORT] [-i INTERFACE]"
+
 def main():
     from mwlib.async.rpcserver import request_handler, server
 
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:i:h", ["port", "interface"])
+    except getopt.GetoptError, err:
+        print str(err)
+        sys.exit(10)
+
+    port = 14311
+    interface = "0.0.0.0"
+    
+    for o, a in opts:
+        if o in ("-p", "--port"):
+            try:
+                port = int(a)
+                if port<=0 or port>65535:
+                    raise ValueError("bad port")
+            except ValueError:
+                print "expected positive integer as argument to %s" % o
+                sys.exit(10)
+        elif o in ("-i",  "--interface"):
+            interface = a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+                                  
+    
     d = db()
     
     class handler(request_handler, qplugin):
         workq = d.workq
         db = d
-        
-    server(8080, get_request_handler=handler).run_forever()
-        
+
+    s=server(port, host=interface, get_request_handler=handler)
+    print "listening on %s:%s" % (interface, port)
+    s.run_forever()
+    
 if __name__=="__main__":
     main()
