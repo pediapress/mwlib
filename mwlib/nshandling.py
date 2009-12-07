cb741bea08cf3dbafddcd529ec207fd8f95a8af1
Volker Haas <volker.haas@brainbot.com>
2009-12-07 12:57:10 +0100

    handle ace interwiki link

--- mwlib/nshandling.py
+++ mwlib/nshandling.py
@@ -39,7 +39,7 @@ class ilink(object):
 
 def fix_wikipedia_siteinfo(siteinfo):
     prefixes = [x['prefix'] for x in siteinfo['interwikimap']]
-    for p in "pnb ckb mwl mhr".split():
+    for p in "pnb ckb mwl mhr ace".split():
         
         if p in prefixes:
             return
