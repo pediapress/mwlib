47563f777279fc901dc1fb862f916354a8a63b0b
Ralf Schmitt <ralf@systemexit.de>
2010-01-22 13:41:43 +0100

    relax odfpy requirement a bit

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def main():
     if os.path.exists(distutils.util.convert_path('Makefile')):
         build_deps()   # this is a hg checkout.
 
-    install_requires=["pyparsing>=1.4.11", "odfpy==0.9", "flup>=1.0", "twisted>=8.2", "lockfile>=0.8", "timelib>=0.2", "WebOb>=0.9", "pyPdf>=1.12"]
+    install_requires=["pyparsing>=1.4.11", "odfpy>=0.9, <0.10", "flup>=1.0", "twisted>=8.2", "lockfile>=0.8", "timelib>=0.2", "WebOb>=0.9", "pyPdf>=1.12"]
     if sys.version_info[:2] < (2,5):
         install_requires.append("wsgiref>=0.1.2")
         install_requires.append("elementtree>=1.2.6")
