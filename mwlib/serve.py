f4257d6fab2a23ddad8caa97840d07c5a28dd791
Johannes Beigel <jojo@sirius.local>
2010-02-11 15:48:49 +0100

    set progress to 0%

--- mwlib/serve.py
+++ mwlib/serve.py
@@ -403,7 +403,7 @@ class Application(object):
             if language:
                 args.extend(['--language', language])
         
-        Status(status_path)(status='job queued')
+        Status(status_path)(status='job queued', progres=0)
         self.queue_render_job('render', collection_id, args)
         
         return response
