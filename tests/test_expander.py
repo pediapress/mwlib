f298730d2257e5f0d98518fae802d291aa8e7391
Ralf Schmitt <ralf@systemexit.de>
2010-09-08 13:38:56 +0200

    test whitespace and implicit newlines in template arguments.
    ,---- http://code.pediapress.com/wiki/ticket/877 ---
    | wow, we strip whitespace when passing unnamed arguments in
    | templates. apparently people don't rely on it, otherwise we should
    | have noticed it a long time ago.
    `----

--- tests/test_expander.py
+++ tests/test_expander.py
@@ -281,7 +281,20 @@ def test_implicit_newline_inner():
     yield expandstr, "ab {{#if: 1| cd {{#if:||#f9f9f9}}}} ef", "ab cd \n#f9f9f9 ef"
     yield expandstr, "ab {{#switch: 1| 1=cd {{#if:||#f9f9f9}}}} ef", "ab cd \n#f9f9f9 ef"
     yield expandstr, "ab{{#tag:imagemap|{{#if:1|#abc}} }}ef", "ab<imagemap>\n#abc </imagemap>ef"
-    
+
+def test_implicit_newline_param():
+    """http://code.pediapress.com/wiki/ticket/877"""
+    wikidb=DictDB(dict(foo="foo{{{1}}}", bar="{|", baz="|"))
+    def doit(a, b):
+        expandstr(a, b, wikidb=wikidb)
+
+    yield doit, "{{foo|{{bar}}}}", "foo\n{|"
+    yield doit, "{{foo|1={{bar}}}}", "foo{|"
+    yield doit, "{{foo|{{{baz}} }}", "foo{| "
+    yield doit, "{{foo|\nbar\n}}baz", "foo\nbar\nbaz"
+    yield doit, "{{foo| bar }}baz", "foo bar baz"
+
+
 def test_expand_after_named():
     db = DictDB(
         show="{{{1}}}",
