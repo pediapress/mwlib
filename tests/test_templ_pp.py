bd88ced11fec351c75a3da62fe03943c31dce62e
Ralf Schmitt <ralf@systemexit.de>
2011-08-26 15:34:43 +0200

    cleanup whitespace

--- tests/test_templ_pp.py
+++ tests/test_templ_pp.py
@@ -5,17 +5,18 @@ from mwlib.templ import pp
 
 def preprocess(s, expected, included=True):
     res = pp.preprocess(s, included=included)
-    
     print "preprocess(%r) -> %r" % (s, res)
     if expected is not None:
-        assert res==expected, "bad preprocess result"
-        
+        assert res == expected, "bad preprocess result"
+
+
 def test_includeonly_included():
     d = lambda s, e: preprocess(s, e, included=False)
     yield d, "foo<includeonly>bar baz\n\n\nbla</includeonly>123<includeonly>foo bar</includeonly>456", "foo123456"
     yield d, "foo<includeonly>bar baz\n\n\nbla", "foo"
     yield d, "foo<ONLYINCLUDE>123</onlyinclude>456", "foo123456"
     yield d, "foo<NOINCLUDE>123</noinclude>456", "foo123456"
+
 
 def test_noinclude_ws():
     yield preprocess, "<noinclude>foo</noinclude>", "", True
