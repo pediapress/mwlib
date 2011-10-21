97a047ea91fe104d2f6e8cdd3749d18e202bf346
Ralf Schmitt <ralf@systemexit.de>
2011-10-21 12:06:54 +0200

    add test for choose_idle_qserve

--- sandbox/test_nserve.py
+++ sandbox/test_nserve.py
@@ -2,6 +2,7 @@
 
 import nserve
 
+
 def pytest_funcarg__app(request):
     tmpdir = request.getfuncargvalue("tmpdir")
     app = nserve.Application(tmpdir.strpath)
@@ -23,7 +24,19 @@ def test_check_collection_id(app):
     cc = app.check_collection_id
     assert not cc("a" * 15)
     assert not cc("a" * 17)
-    assert cc("a"*16)
+    assert cc("a" * 16)
     assert not cc("A" * 16)
     assert cc("0123456789abcdef")
     assert not cc("g" * 16)
+
+
+def test_choose_idle_qserve(monkeypatch):
+    busy = {}
+    monkeypatch.setattr(nserve, "busy", busy)
+    assert nserve.choose_idle_qserve() is None
+    busy["host1"] = True
+    assert nserve.choose_idle_qserve() is None
+    busy["host2"] = False
+    assert nserve.choose_idle_qserve() == "host2"
+    busy["host1"] = False
+    assert set(nserve.choose_idle_qserve() for i in range(20)) == set(["host1", "host2"])
