5d1cc5ffec7b7410ebfb84e9c1fc9ad2cfb8df20
Ralf Schmitt <ralf@systemexit.de>
2011-10-21 15:30:27 +0200

    add some more tests to test_nserve
    exercise _iterate a bit and test that the __call__ is killable with
    GreenletExit.

--- sandbox/test_nserve.py
+++ sandbox/test_nserve.py
@@ -1,6 +1,14 @@
 #! /usr/bin/env py.test
 
-import nserve
+import pytest, gevent, nserve, time
+
+
+def get_exception_raiser(msg, exception_class=RuntimeError):
+    def raise_exc(*args, **kwargs):
+        raise exception_class(msg)
+    return raise_exc
+
+raise_greenletexit = get_exception_raiser("killed", gevent.GreenletExit)
 
 
 def pytest_funcarg__app(request):
@@ -14,6 +22,15 @@ def pytest_funcarg__busy(request):
     monkeypatch = request.getfuncargvalue("monkeypatch")
     monkeypatch.setattr(nserve, "busy", busy)
     return busy
+
+
+def pytest_funcarg__wq(request):
+    busy = request.getfuncargvalue("busy")
+    wq = nserve.watch_qserve(("localhost", 8888), busy)
+    wq.getstats_timeout = 0.01
+    wq.sleep_time = 0.01
+    busy[wq.ident] = True
+    return wq
 
 
 # -- tests
@@ -45,3 +62,26 @@ def test_choose_idle_qserve(monkeypatch, busy):
     assert nserve.choose_idle_qserve() == "host2"
     busy["host1"] = False
     assert set(nserve.choose_idle_qserve() for i in range(20)) == set(["host1", "host2"])
+
+
+def test_watch_qserve_iterate_overloaded(busy, wq):
+    wq._getstats = lambda *args: dict(busy=dict(render=11))
+    wq._iterate()
+    assert busy[wq.ident] == "system overloaded"
+
+
+def test_watch_qserve_iterate_down(busy, wq):
+    wq._getstats = get_exception_raiser("getstats failed")
+    wq._iterate()
+    assert busy[wq.ident] == "system down"
+
+
+def test_watch_qserve_iterate_killable(busy, wq):
+    wq._getstats = raise_greenletexit
+    pytest.raises(gevent.GreenletExit, wq._iterate)
+
+
+def test_watch_qserve_call_killable(wq):
+    wq._getstats = raise_greenletexit
+    wq._sleep = get_exception_raiser("do not call sleep")
+    pytest.raises(gevent.GreenletExit, wq)
