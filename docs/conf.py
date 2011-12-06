6cdc691643df25fca749cb3ab43905478b3c705a
Ralf Schmitt <ralf@systemexit.de>
2011-12-06 11:52:39 +0100

    fix copyright

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'mwlib'
-copyright = u'2011, Volker Haas, Ralf Schmitt, Johannes Beigel'
+copyright = u'PediaPress GmbH'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
