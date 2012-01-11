062cc867a91ffbb6bc310e18b93cc14254d7f99f
Ralf Schmitt <ralf@systemexit.de>
2012-01-11 22:37:56 +0100

    up version to 0.13.2

--- mwlib/_version.py
+++ mwlib/_version.py
@@ -5,7 +5,7 @@ class _Version(tuple):
     def __str__(self):
         return '.'.join([str(x) for x in self])
 
-version = _Version((0, 13, 1))
+version = _Version((0, 13, 2))
 del _Version
 
 try:
