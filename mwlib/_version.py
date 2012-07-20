18985a8f26be5b272800dde6bfc5a88e924fcd54
Ralf Schmitt <ralf@systemexit.de>
2012-07-20 12:57:18 +0200

    get rid of the _Version class, up version to 0.14.0
    also move mw-version's main to mw-version

--- mwlib/_version.py
+++ mwlib/_version.py
@@ -1,42 +1,9 @@
-class _Version(tuple):
-    """internal version object, subclass of C{tuple},
-    but implements a fancier __str__ representation
-    """
-    def __str__(self):
-        return '.'.join([str(x) for x in self])
-
-version = _Version((0, 13, 11))
-del _Version
+__version_info__ = (0, 14, 0)
+version = __version__ = "0.14.0"
 
 try:
     from mwlib._gitversion import gitid, gitversion
 except ImportError:
     gitid = gitversion = ""
 
-display_version = gitversion or str(version)
-
-def main():
-    print "mwlib: %s (%s)" % (display_version, gitid)
-    try:
-        from mwlib.rl import _gitversion
-        print "mwlib.rl", _gitversion.gitversion
-    except ImportError:        
-        try:
-            from mwlib.rl._version import version as rlversion
-            print "mwlib.rl:", rlversion
-        except ImportError:
-            pass
-
-    try:
-        from mwlib._extversion import version as extversion
-        print "mwlib.ext:", extversion
-    except ImportError:
-        pass
-
-    try:
-        from mwlib.hiq import _gitversion
-    except ImportError:
-        pass
-    else:
-        print "mwlib.hiq",  _gitversion.gitversion
-        
+display_version = gitversion or version
