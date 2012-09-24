bbe0572c95cd3c0fee52d59d6d739a6f864913c4
Ralf Schmitt <ralf@systemexit.de>
2012-09-24 11:13:47 +0200

    cleanup whitespace in magic_nodes.py

--- mwlib/templ/magic_nodes.py
+++ mwlib/templ/magic_nodes.py
@@ -1,13 +1,15 @@
 from xml.sax.saxutils import quoteattr
 from mwlib.templ import nodes, evaluate
 
+
 class Subst(nodes.Node):
     def flatten(self, expander, variables, res):
         name = []
         evaluate.flatten(self[0], expander, variables, name)
         name = u"".join(name).strip()
-        
+
         res.append("{{subst:%s}}" % (name,))
+
 
 class Safesubst(nodes.Template):
     def _get_args(self):
@@ -20,9 +22,7 @@ class Time(nodes.Node):
         evaluate.flatten(self[0], expander, variables, format)
         format = u"".join(format).strip()
 
-
-        
-        if len(self)>1:
+        if len(self) > 1:
             d = []
             evaluate.flatten(self[1], expander, variables, d)
             d = u"".join(d).strip()
@@ -31,6 +31,7 @@ class Time(nodes.Node):
 
         from mwlib.templ import magic_time
         res.append(magic_time.time(format, d))
+
 
 class Anchorencode(nodes.Node):
     def flatten(self, expander, variables, res):
@@ -45,8 +46,9 @@ class Anchorencode(nodes.Node):
         e = urllib.quote_plus(arg.encode('utf-8'), ':').replace('%', '.').replace('+', '_')
         res.append(e)
 
+
 def _rel2abs(rel, base):
-    rel=rel.rstrip("/")
+    rel = rel.rstrip("/")
     if rel in (u"", "."):
         return base
     if not (rel.startswith("/") or rel.startswith("./") or rel.startswith("../")):
@@ -56,7 +58,7 @@ def _rel2abs(rel, base):
     p = posixpath.normpath("/%s/%s/" % (base, rel)).strip("/")
     return p
 
-    
+
 class rel2abs(nodes.Node):
     def flatten(self, expander, variables, res):
         arg = []
@@ -64,14 +66,15 @@ class rel2abs(nodes.Node):
         arg = u"".join(arg).strip()
 
         arg2 = []
-        if len(self)>1:
+        if len(self) > 1:
             evaluate.flatten(self[1], expander, variables, arg2)
         arg2 = u"".join(arg2).strip()
         if not arg2:
             arg2 = expander.pagename
 
         res.append(_rel2abs(arg, arg2))
-        
+
+
 class Tag(nodes.Node):
     def flatten(self, expander, variables, res):
         name = []
@@ -90,27 +93,28 @@ class Tag(nodes.Node):
 
         tmpres = []
         tmpres.append("<%s%s>" % (name, parameters))
-        
-        if len(self)>1:
+
+        if len(self) > 1:
             tmp = []
             evaluate.flatten(self[1], expander, variables, tmp)
             evaluate._insert_implicit_newlines(tmp)
             tmp = u"".join(tmp)
             tmpres.append(tmp)
-            
+
         tmpres.append("</%s>" % (name,))
         tmpres = u"".join(tmpres)
         tmpres = expander.uniquifier.replace_tags(tmpres)
         res.append(tmpres)
-        
-        
+
 
 class NoOutput(nodes.Node):
     def flatten(self, expander, variables, res):
         pass
 
+
 class Defaultsort(NoOutput):
     pass
+
 
 class Displaytitle(nodes.Node):
     def flatten(self, expander, variables, res):
@@ -118,7 +122,7 @@ class Displaytitle(nodes.Node):
         evaluate.flatten(self[0], expander, variables, name)
         name = u"".join(name).strip()
         expander.magic_displaytitle = name
-        
+
 
 def make_switchnode(args):
     return nodes.SwitchNode((args[0], args[1:]))
@@ -131,7 +135,7 @@ registry = {'#time': Time,
             'displaytitle': Displaytitle,
             'defaultsort': Defaultsort,
             '#rel2abs': rel2abs,
-            '#switch': make_switchnode, 
+            '#switch': make_switchnode,
             '#if': nodes.IfNode,
             '#ifeq': nodes.IfeqNode,
             }
