90731892d6b5ea8259d3c4b947df0c29f021f1e3
Ralf Schmitt <ralf@systemexit.de>
2013-03-13 11:41:45 +0100

    up version to 0.15.2

--- mwlib/_version.py
+++ mwlib/_version.py
@@ -1,5 +1,5 @@
-__version_info__ = (0, 15, 1)
-version = __version__ = "0.15.1"
+__version_info__ = (0, 15, 2)
+version = __version__ = "0.15.2"
 
 try:
     from mwlib._gitversion import gitid, gitversion
