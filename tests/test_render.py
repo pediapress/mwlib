6b512f48faf69b06c6b34b5225ce3253b118eed6
Ralf Schmitt <ralf@systemexit.de>
2014-01-13 16:03:19 +0100

    prevent race conditions in rlwriter.toc by using a tmp directory

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,38 +1,34 @@
 #! /usr/bin/env py.test
 
-import os
-import tempfile
-import glob
+import os, glob, pytest
 
 
-def pytest_generate_tests(metafunc):
-    zipfiles = glob.glob(os.path.abspath(os.path.join(os.path.dirname(__file__),  "*.zip")))
-    for name in writer_names():
-        for z in zipfiles:
-            metafunc.addcall(id="%s:%s" % (name, os.path.basename(z)), funcargs=dict(writer=name, input=z))
+def zipfiles():
+    return glob.glob(os.path.abspath(os.path.join(os.path.dirname(__file__), "*.zip")))
 
 
 def writer_names():
     import pkg_resources
-    retval = []
     for x in pkg_resources.iter_entry_points("mwlib.writers"):
         try:
             x.load()
+            yield x.name
         except Exception:
             pass
-        else:
-            retval.append(x.name)
-
-    return retval
 
 
-def test_render(writer, input):
-    tmp = tempfile.mktemp()
-    cmd = "mw-render -w %s -c %s -o %s" % (writer, input, tmp)
+@pytest.fixture(params=writer_names())
+def writer(request):
+    return request.param
+
+
+@pytest.fixture(params=zipfiles())
+def zipfile(request):
+    return request.param
+
+
+def test_render(writer, zipfile, tmpdir):
+    cmd = "mw-render -w %s -c %s -o %s" % (writer, zipfile, tmpdir.join("output"))
     print "running", cmd
-    try:
-        err = os.system(cmd)
-        assert err == 0
-    finally:
-        if os.path.exists(tmp):
-            os.unlink(tmp)
+    err = os.system(cmd)
+    assert err == 0
