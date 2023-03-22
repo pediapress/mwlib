dc68315d7999636220048860a45af55216e86cd9
Christoph Kepper <christoph@pediapress.com>
2023-03-23 00:53:45 +0100

    use version number from pyproject.toml in _version.py

--- src/mwlib/_version.py
+++ src/mwlib/_version.py
@@ -1,13 +1,36 @@
-__version_info__ = (0, 16, 0)
-display_version = version = __version__ = "0.16.0"
+import importlib.metadata
+from pathlib import Path
+
+import toml
+
+root_dir = Path(__file__).resolve().parent.parent.parent
+
+
+def get_version_from_pyproject():
+    with open(root_dir / "pyproject.toml", encoding="utf-8") as file:
+        pyproject_data = toml.load(file)
+        v = pyproject_data["project"]["version"]
+        return v
+
+
+def get_version_from_package(package_name):
+    try:
+        return importlib.metadata.version(package_name)
+    except importlib.metadata.PackageNotFoundError:
+        return None
+
+
+version = get_version_from_pyproject()
+__version_info__ = tuple(map(int, version.split(".")))
+display_version = __version__ = version
 
 
 def main():
-    import pkg_resources
-
     for r in ("mwlib", "mwlib.rl", "mwlib.ext", "mwlib.hiq"):
-        try:
-            v = pkg_resources.require(r)[0].version
+        v = get_version_from_package(r)
+        if v:
             print(r, v)
-        except BaseException:
-            continue
+
+
+if __name__ == "__main__":
+    main()
