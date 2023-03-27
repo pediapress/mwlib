54443253d1108d1000f58275f58fb37d583eff4a
Christoph Kepper <christoph@pediapress.com>
2023-03-27 22:35:14 +0200

    refactor pp.py
    * remove absolute_import
    * make regexes raw strings

--- src/mwlib/templ/pp.py
+++ src/mwlib/templ/pp.py
@@ -1,8 +1,5 @@
 # Copyright (c) 2007-2009 PediaPress GmbH
 # See README.md for additional licensing information.
-
-from __future__ import absolute_import
-
 import re
 
 
@@ -10,14 +7,14 @@ def rxc(s):
     return re.compile(s, re.DOTALL | re.IGNORECASE)
 
 
-onlyincluderx = rxc("<onlyinclude>(.*?)</onlyinclude>")
-noincluderx = rxc("<noinclude(?:\s[^<>]*)?>.*?(</noinclude>|$)")
-includeonlyrx = rxc("<includeonly(?:\s[^<>]*)?>.*?(?:</includeonly>|$)")
+onlyincluderx = rxc(r"<onlyinclude>(.*?)</onlyinclude>")
+noincluderx = rxc(r"<noinclude(?:\s[^<>]*)?>.*?(</noinclude>|$)")
+includeonlyrx = rxc(r"<includeonly(?:\s[^<>]*)?>.*?(?:</includeonly>|$)")
 
 
 def get_remove_tags(tags):
-    r = rxc("</?(%s)(?:\s[^<>]*)?>" % ("|".join(tags)))
-    return lambda s: r.sub(u"", s)
+    r = rxc(r"</?(%s)(?:\s[^<>]*)?>" % ("|".join(tags)))
+    return lambda s: r.sub("", s)
 
 
 remove_not_included = get_remove_tags(["onlyinclude", "noinclude"])
@@ -25,13 +22,13 @@ remove_not_included = get_remove_tags(["onlyinclude", "noinclude"])
 
 def preprocess(txt, included=True):
     if included:
-        txt = noincluderx.sub(u"", txt)
+        txt = noincluderx.sub("", txt)
 
         if "<onlyinclude>" in txt:
             # if onlyinclude tags are used, only use text between those tags. template 'legend' is a example
             txt = "".join(onlyincluderx.findall(txt))
     else:
-        txt = includeonlyrx.sub(u"", txt)
+        txt = includeonlyrx.sub("", txt)
         txt = remove_not_included(txt)
 
     return txt
